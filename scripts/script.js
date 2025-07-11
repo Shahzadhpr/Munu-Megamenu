9d088c68965328d8bed441f443612a19f231c1c3
Shahzad <hprshahzad29@gmail.com>
2025-07-11 18:58:41 +0330

    Implementing a mobile shopping cart structure

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,12 @@
-// Overlay
 let overlay = document.querySelector(".overlay");
 let menuItem = document.querySelector(".menu__item--hassub");
+let header = document.querySelector(".header");
+let cartBtn = document.querySelector(".cart__btn");
+let cartBody = document.querySelector(".cart__body");
+let overlayCart = document.querySelector(".overlay-cart");
+let mobilCarteBtn = document.querySelector(".mobile-cart__btn");
+let mobileCart = document.querySelector(".mobile-cart");
+let mobileCartCloseBtn = document.querySelector(".mobile-cart__close-btn");
 
 menuItem.addEventListener("mouseover", function (){
     overlay.classList.add("overlay--show")
@@ -8,6 +14,31 @@ menuItem.addEventListener("mouseover", function (){
 
 menuItem.addEventListener("mouseout", function (){
     overlay.classList.remove("overlay--show")
+})
+
+
+cartBtn.addEventListener("click", function (){
+    cartBody.classList.add("cart__body--active");
+    overlayCart.classList.add("overlay--show");
+    header.style.zIndex = "auto";
+})
+
+overlayCart.addEventListener("click", function (){
+    cartBody.classList.remove("cart__body--active");
+    overlayCart.classList.remove("overlay--show");
+    setTimeout(function (){
+        header.style.zIndex = "20";
+    },100)
+})
+
+mobilCarteBtn.addEventListener("click", function (){
+    mobileCart.classList.add("mobile-cart--open");
+    overlay.classList.add("overlay--show");
+})
+
+mobileCartCloseBtn.addEventListener("click", function (){
+    mobileCart.classList.remove("mobile-cart--open");
+    overlay.classList.remove("overlay--show");
 })
 
 
@@ -28,24 +59,3 @@ document.querySelector('.submenu').addEventListener('mouseleave', function() {
     });
     document.querySelector('.submenu__item-wrapper:first-child .submenu__item-body').style.display = 'flex';
 });
-
-
-// Overlay-cart
-let header = document.querySelector(".header");
-let cartBtn = document.querySelector(".cart__btn");
-let cartBody = document.querySelector(".cart__body");
-let overlayCart = document.querySelector(".overlay-cart");
-
-cartBtn.addEventListener("click", function (){
-    cartBody.classList.add("cart__body--active");
-    overlayCart.classList.add("overlay--show");
-    header.style.zIndex = "auto";
-})
-
-overlayCart.addEventListener("click", function (){
-    cartBody.classList.remove("cart__body--active");
-    overlayCart.classList.remove("overlay--show");
-    setTimeout(function (){
-        header.style.zIndex = "20";
-    },100)
-})
