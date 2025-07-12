19a145687b601fd6c9a7bb49f339e526c848357c
Shahzad <hprshahzad29@gmail.com>
2025-07-12 16:08:14 +0330

    Project completion :)

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,13 @@ let overlayCart = document.querySelector(".overlay-cart");
 let mobilCarteBtn = document.querySelector(".mobile-cart__btn");
 let mobileCart = document.querySelector(".mobile-cart");
 let mobileCartCloseBtn = document.querySelector(".mobile-cart__close-btn");
+let navMenuSubIcon = document.querySelector(".nav-menu__sub-icon");
+let navMenuSubmenu = document.querySelector(".nav-menu__submenu");
+let navMenuCloseBtn = document.querySelector(".nav-menu__close-btn");
+let navMenu = document.querySelector(".nav__menu");
+let navBtn = document.querySelector(".nav-btn");
+let overlayNav = document.querySelector(".overlay-nav");
+
 
 menuItem.addEventListener("mouseover", function (){
     overlay.classList.add("overlay--show")
@@ -41,6 +48,28 @@ mobileCartCloseBtn.addEventListener("click", function (){
     overlay.classList.remove("overlay--show");
 })
 
+navMenuSubIcon.addEventListener("click", function (){
+    navMenuSubmenu.classList.toggle("nav-menu__submenu--open")
+})
+
+navMenuSubIcon.addEventListener("click", function (){
+    navMenuSubIcon.classList.toggle("nav-menu__sub-icon--open")
+})
+
+navBtn.addEventListener("click", function (){
+    navMenu.classList.add("nav__menu--open");
+    overlayNav.classList.add("overlay-nav--show");
+});
+
+navMenuCloseBtn.addEventListener("click", function (){
+    navMenu.classList.remove("nav__menu--open");
+    overlayNav.classList.remove("overlay-nav--show");
+});
+
+overlayNav.addEventListener("click", function (){
+    navMenu.classList.remove("nav__menu--open");
+    overlayNav.classList.remove("overlay-nav--show");
+});
 
 //product-item
 document.querySelectorAll('.submenu__item-wrapper').forEach(item => {
